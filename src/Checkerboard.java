5b18370087672a36aaefcc4e609953a6ca49c506
swisskanton <kormanyos.antal@gmail.com>
2023-03-21 16:30:18 +0100

    Drawing and Graphics

---
+++ src/Checkerboard.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Checkerboard {
+    public static void drawImage(Graphics graphics) {
+        // Fill the canvas with a checkerboard pattern
+
+        int size = WIDTH / 8;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                graphics.setColor(((i + j) % 2 == 0) ? Color.white : Color.black);
+                graphics.fillRect(i * size, j * size, size, size);
+            }
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            drawImage(graphics);
+        }
+    }
+}
