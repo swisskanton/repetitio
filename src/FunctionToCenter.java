e1e5c7d480b714b017b9ea29d32fc58ea3c6cd88
swisskanton <kormanyos.antal@gmail.com>
2023-03-21 16:39:00 +0100

    Drawing and Graphics

---
+++ src/FunctionToCenter.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FunctionToCenter {
+    public static void drawImage(Graphics graphics) {
+        // Create a function that takes 3 parameters and draws a single line
+        // Parameters: the x and y coordinates of the line's starting point and the graphics
+        // The function shall draw a line from that point to the center of the canvas
+        // Fill the canvas with lines from the edges (every 20 px) to the center
+
+        for (int i = 0; i <= WIDTH; i+= 20) {
+            draeToCenter(i, 0, graphics);
+            draeToCenter(i, HEIGHT, graphics);
+            draeToCenter(0, i, graphics);
+            draeToCenter(WIDTH, i, graphics);
+        }
+    }
+
+    public static void draeToCenter(int x, int y, Graphics graphics) {
+        graphics.drawLine(x, y, WIDTH / 2, HEIGHT / 2);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            drawImage(graphics);
+        }
+    }
+}
